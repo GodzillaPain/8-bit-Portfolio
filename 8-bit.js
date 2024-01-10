8d05f9c62923eb56df00b4891f5574c128a01233
GodzillaPain <davidkalajdzic0@gmail.com>
2024-01-10 15:50:36 -0500

    toggle menu

---
+++ 8-bit.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.style.display = (menu.style.display === "none" || menu.style.display === "") ? "block" : "none";
+}
+
