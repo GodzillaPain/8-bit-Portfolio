f128ce6750988016be15e5e5a62661a329670f81
GodzillaPain <davidkalajdzic0@gmail.com>
2024-01-11 09:00:26 -0500

    Update 8-bit.js

--- 8-bit.js
+++ 8-bit.js
@@ -5,25 +5,27 @@ function toggleMenu(event) {
     var title = document.querySelector(".title");
     var screenContainer = document.querySelector(".screen-container");
 
-    // Check if the click event occurred on the button
     if (event.target.id === 'startButton') {
-        console.log("Before toggling - Menu display: ", menu.style.display);
-        console.log("Before toggling - Title display: ", title.style.display);
         menu.style.display = (menu.style.display === "none" || menu.style.display === "") ? "block" : "none";
-        title.style.display = (title.style.display === "none" || title.style.display === "") ? "block" : "none";
-        console.log("After toggling - Menu display: ", menu.style.display);
-        console.log("After toggling - Title display: ", title.style.display);
     }
 }
-    document.addEventListener('DOMContentLoaded', function () {
+
+document.addEventListener('DOMContentLoaded', function () {
     var menuLinks = document.querySelectorAll('#menu a');
     var screenContainer = document.querySelector('.screen-container');
-
+  
     menuLinks.forEach(function (link) {
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
-            screenContainer.classList.toggle("expanded");
-        });
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
+        screenContainer.classList.toggle("expanded");
+  
+        // Toggle the display of the element with class 'title'
+        var title = document.querySelector('.title');
+        if (title) {
+          title.style.display = (title.style.display === 'none' || title.style.display === '') ? 'block' : 'none';
+        }
+      });
     });
-});
+  });
+  
 
