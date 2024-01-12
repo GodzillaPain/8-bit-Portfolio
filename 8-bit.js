431f995db3546141c960a153c9966b470bae1ac3
GodzillaPain <davidkalajdzic0@gmail.com>
2024-01-12 10:53:19 -0500

    fix js

--- 8-bit.js
+++ 8-bit.js
@@ -1,31 +1,41 @@
 function toggleMenu(event) {
-    console.log("Function called");
+  console.log("Function called");
 
-    var menu = document.getElementById("menu");
-    var title = document.querySelector(".title");
-    var screenContainer = document.querySelector(".screen-container");
+  var menu = document.getElementById("menu");
+  var title = document.querySelector(".title");
+  var screenContainer = document.querySelector(".screen-container");
+  var aboutPage = document.getElementById("about-me");
 
-    if (event.target.id === 'startButton') {
-        menu.style.display = (menu.style.display === "none" || menu.style.display === "") ? "block" : "none";
-    }
+  if (event.target.id === 'startButton') {
+    menu.style.display = (menu.style.display === "none" || menu.style.display === "") ? "block" : "none";
+
+
+    title.classList.toggle("hide");
+
+  }
 }
 
+
 document.addEventListener('DOMContentLoaded', function () {
-    var menuLinks = document.querySelectorAll('#menu a');
-    var screenContainer = document.querySelector('.screen-container');
-  
-    menuLinks.forEach(function (link) {
-      link.addEventListener('click', function (event) {
-        event.preventDefault();
-        screenContainer.classList.toggle("expanded");
-  
-        // Toggle the display of the element with class 'title'
-        var title = document.querySelector('.title');
-        if (title) {
-          title.style.display = (title.style.display === 'none' || title.style.display === '') ? 'block' : 'none';
-        }
-      });
+  var menuLinks = document.querySelectorAll('#menu a');
+  var screenContainer = document.querySelector('.screen-container');
+  var aboutPage = document.getElementById('about-me');
+  var menu = document.getElementById('menu');
+
+  menuLinks.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      // Toggle the 'expanded' class on screenContainer
+      screenContainer.classList.toggle('expanded');
+
+      // Hide the menu
+      menu.style.display = 'none';
+
+      // Toggle the 'hide' class on aboutPage
+      aboutPage.classList.toggle('hide');
     });
   });
-  
+});
+
 
